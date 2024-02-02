4cea8be66c59196ed0543dc35ac04a9ee0886f18
zarathushtraa <156239772+zarathushtraa@users.noreply.github.com>
2024-02-02 20:33:56 -0300

    made a few more changes to solve problems

--- operations.js
+++ operations.js
@@ -8,7 +8,8 @@ function multiply (a, b){
     return a * b;
 };
 function divide (a, b){
-    return a / b;
+    if (b === 0) return 'Snarky ErRoR';
+    else return a / b;
 };
 function operate (a, op, b){
     if (op === '+') {
@@ -31,7 +32,7 @@ let lastdigitOp = 0;
 let totaldigits = 0;
 function display (variable){
     const screen = document.querySelector("#display");
-    if(screen.textContent === 'ERROR' || totaldigits > 10) return (screen.textContent = 'ERROR');
+    if(screen.textContent === 'ERROR' || totaldigits > 10 ) return (screen.textContent = 'ERROR');
     if (Number(variable) === variable){
         if(lastdigitNum === 1){
             let con = numbersArray[(numbersArray.length-1)].toString();
@@ -74,19 +75,24 @@ const equals = document.querySelector('#equals');
 equals.addEventListener('click', () => {
     const screen = document.querySelector("#display");
     if(screen.textContent === 'ERROR') return (screen.textContent = 'ERROR');
+    if (screen.textContent === '') return (screen.textContent = 'nothing here :/');
     while (numbersArray.length > 1){
         let store = operate(numbersArray[0],operatorsArray[0],numbersArray[1]);
+        console.log(store);
         numbersArray.shift();
         numbersArray.shift();
         numbersArray.unshift(store);
         operatorsArray = operatorsArray.slice(1);
     };
-    screen.textContent = +numbersArray[0].toFixed(5) + operatorsArray;
-    if (numbersArray[0].toFixed(5).toString().length > numbersArray[0].toString().length){
-        totaldigits = numbersArray[0].toString().length;
-    }
-    else{
-        totaldigits = numbersArray[0].toFixed(5).toString().length;
+    if (numbersArray[0] === 'Snarky ErRoR' || screen.textContent === 'nothing here :/') screen.textContent = 'try again lol';
+    else {
+        screen.textContent = +numbersArray[0].toFixed(5) + operatorsArray;
+        if (numbersArray[0].toFixed(5).toString().length > numbersArray[0].toString().length){
+            totaldigits = numbersArray[0].toString().length;
+        }
+        else{
+            totaldigits = numbersArray[0].toFixed(5).toString().length;
+        }
     }
 
 });
