1e45bfbc20824e628c6e6974e776e1e566ee75af
zarathushtraa <156239772+zarathushtraa@users.noreply.github.com>
2024-02-01 22:18:12 -0300

    added funcs for display correct & fix erase

--- operations.js
+++ operations.js
@@ -29,3 +29,61 @@ function operate (a, op, b){
     }
 
 }
+
+let operatorsArray = [];
+let numbersArray = [];
+let lastdigitNum = 0;
+let lastdigitOp = 0;
+let totaldigits = 0;
+
+function display (variable){
+    const screen = document.querySelector("#display");
+    if(screen.textContent === 'ERROR' || totaldigits > 10) return (screen.textContent = 'ERROR');
+    if (Number(variable) === variable){
+        if(lastdigitNum === 1){
+            let con = numbersArray[(numbersArray.length-1)].toString();
+            numbersArray[(numbersArray.length-1)] = Number(con + variable);
+            console.log(numbersArray);
+        }else{
+        numbersArray.push(variable);
+        console.log(numbersArray);
+        }
+        lastdigitNum = 1;
+        lastdigitOp = 0;
+        totaldigits += 1;
+    }else {
+        if(lastdigitOp === 1) return (screen.textContent = 'ERROR');
+        operatorsArray.push(variable);
+        console.log(operatorsArray);
+        lastdigitNum = 0;
+        lastdigitOp = 1;
+        totaldigits += 1;
+    }
+
+    
+    screen.textContent += variable;
+    
+}
+
+const nums = document.querySelectorAll ('.number');
+for (const num of nums){
+    num.addEventListener('click', () => display(Number(num.textContent)));
+}
+
+const opes = document.querySelectorAll ('.operator');
+for (const ope of opes){
+    ope.addEventListener('click', () => display(ope.textContent));
+}
+
+
+const eraseButton = document.querySelector('#AC');
+eraseButton.addEventListener('click', ()=> {
+    const screen = document.querySelector("#display");
+    screen.textContent = '';
+    totaldigits = 0;
+    operatorsArray = [];
+    numbersArray = [];
+    lastdigitNum = 0;
+    lastdigitOp = 0;
+});
+
